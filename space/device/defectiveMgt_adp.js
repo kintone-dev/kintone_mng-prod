232d83a26cde933f1c857df4f7bb394310e9b983
kintone-mng <89760706+kintone-mng@users.noreply.github.com>
2021-11-16 07:46:16 +0900

    Update defectiveMgt_adp.js

--- space/device/defectiveMgt_adp.js
+++ space/device/defectiveMgt_adp.js
@@ -5,40 +5,54 @@
     let nStatus=event.nextStatus.value;
     console.log(nStatus);
     if(nStatus=='検証完了'){
-      let putDefectiveMgtData=[];
-      let putDefectiveMgtBody={
-        'updateKey': {
-          'field': 'sNum',
-          'value': event.record.sNum.value
-        },
-        'record': {}
-      }
-      let defectiveJudgment=event.record.defectiveJudgment.value;
-      switch(defectiveJudgment){
-        case '故障あり':
-          putDefectiveMgtBody.record={
-            'sState':{'value': '故障品'},
-            'sDstate':{'value': event.record.sDstate.value},
-            'Verifier':{'value': event.record.Verifier.value}
-          }
-          break;
-        case '故障なし':
-          putDefectiveMgtBody.record={
-            'sState':{'value': '再生品'},
-            'sDstate':{'value': ''},
-            'Verifier':{'value': event.record.Verifier.value}
-          }
-          break;
-        case '判定不可':
-          putDefectiveMgtBody.record={
-            'sState':{'value': '判定不可'},
-            'sDstate':{'value': ''},
-            'Verifier':{'value': event.record.Verifier.value}
-          }
-          break;
-      }
-      putDefectiveMgtData.push(putDefectiveMgtBody);
-      await putRecords(sysid.DEV.app_id.sNum, putDefectiveMgtData);
+      let getSN_sState={
+        'app':sysid.DEV.app_id.sNum,
+        'query':'sNum="'+event.record.sNum.value+'" and sState not in ("","使用中","正常品","再生品")'
+      };
+      kintone.api(kintone.api.url('/k/v1/records', true), 'GET', getSN_sState).then(function(resp){
+        let putDefectiveMgtData=[];
+        let putDefectiveMgtBody={
+          'updateKey': {
+            'field': 'sNum',
+            'value': event.record.sNum.value
+          },
+          'record': {}
+        }
+        let defectiveJudgment=event.record.defectiveJudgment.value;
+        switch(defectiveJudgment){
+          case '故障あり':
+            putDefectiveMgtBody.record={
+              'sState':{'value': '故障品'},
+              'sDstate':{'value': event.record.sDstate.value},
+              'Verifier':{'value': event.record.Verifier.value}
+            }
+            break;
+          case '故障なし':
+            putDefectiveMgtBody.record={
+              'sState':{'value': '再生品'},
+              'sDstate':{'value': ''},
+              'Verifier':{'value': event.record.Verifier.value}
+            }
+            break;
+          case '判定不可':
+            putDefectiveMgtBody.record={
+              'sState':{'value': '判定不可'},
+              'sDstate':{'value': ''},
+              'Verifier':{'value': event.record.Verifier.value}
+            }
+            break;
+        }
+        putDefectiveMgtData.push(putDefectiveMgtBody);
+        await putRecords(sysid.DEV.app_id.sNum, putDefectiveMgtData);
+      }).catch(function(error){
+        console.log(error);
+        return ['error', error];
+      });
+    }
+    if (Array.isArray(getRepResult)) {
+      event.error = '未登録のシリアル番号または故障申告ができてないシリアル番号です。';
+      endLoad();
+      return event;
     }
     endLoad();
     return event;
