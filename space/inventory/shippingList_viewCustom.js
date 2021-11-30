f75dfcdef7d45684fc74611fc6079c1dd7dcf041
kintone-mng <89760706+kintone-mng@users.noreply.github.com>
2021-11-30 17:35:00 +0900

    Update shippingList_viewCustom.js

--- space/inventory/shippingList_viewCustom.js
+++ space/inventory/shippingList_viewCustom.js
@@ -443,12 +443,8 @@
           for(let y in resp.records){
             var macAddress='';
             let remarks=ship_deviceList[i].value.shipRemarks.value;
-            if(y==0){
-              if(remarks==undefined) ship_deviceList[i].value.shipRemarks.value=resp.records[y].macaddress.value;
-              else ship_deviceList[i].value.shipRemarks.value+=resp.records[y].macaddress.value;
-            }else{
-              ship_deviceList[i].value.shipRemarks.value+='\n'+resp.records[y].macaddress.value;
-            }
+            if(y==0 || remarks==undefined) ship_deviceList[i].value.shipRemarks.value=resp.records[y].macaddress.value;
+            else ship_deviceList[i].value.shipRemarks.value+='\n'+resp.records[y].macaddress.value;
           }
           console.log(event.record.deviceList.value);
           return event;
