a04822c44a4f3f7405e35199b8388d9f732995b8
kintone-mng <89760706+kintone-mng@users.noreply.github.com>
2021-12-23 19:29:53 +0900

    Update shippingList_viewCustom.js

--- space/inventory/shippingList_viewCustom.js
+++ space/inventory/shippingList_viewCustom.js
@@ -41,9 +41,7 @@
   });
 
   kintone.events.on(['app.record.create.show', 'app.record.edit.show', 'app.record.detail.show'], function (event) {
-    console.log(event.record.shipType.value);
-    let shiptypeValue=event.record.shipType.value;
-    console.log(shiptypeValue);
+    let shiptypeValue=''//event.record.shipType.value;
     if(shiptypeValue==null||shiptypeValue==undefined||shiptypeValue==''){
       ctl_dstselection(event, 'none', false);
     }else if(shiptypeValue.match(/返品|移動-ベンダー/)){
