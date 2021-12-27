556abf957d49ce27ba76c10e236452d6686f90fe
kintone-mng <89760706+kintone-mng@users.noreply.github.com>
2021-12-27 13:51:02 +0900

    Update shippingList_actionReceive.js

--- space/inventory/shippingList_actionReceive.js
+++ space/inventory/shippingList_actionReceive.js
@@ -4,12 +4,13 @@
   kintone.events.on('app.record.create.show', function (event) {
     event.record.prjNum.disabled = true;
     //コピー元の「prjNum」の値をsessionStorageの値を代入
-    event.record.prjNum.value = sessionStorage.getItem('prjNum');
-    event.record.shipType.value = sessionStorage.getItem('shipType');
-    event.record.tarDate.value = sessionStorage.getItem('tarDate');
-    event.record.instName.value = sessionStorage.getItem('instName');
-    event.record.instName.lookup = true;
+    // event.record.prjNum.value = sessionStorage.getItem('prjNum');
+    // event.record.shipType.value = sessionStorage.getItem('shipType');
+    // event.record.tarDate.value = sessionStorage.getItem('tarDate');
+    // event.record.instName.value = sessionStorage.getItem('instName');
+    // event.record.instName.lookup = true;
     console.log(sessionStorage.getItem('is_copy_shipdata'));
+    // データ複製ボタン受取
     if(sessionStorage.getItem('is_copy_shipdata')){
       event.record=JSON.parse(sessionStorage.getItem('copy_shipdata'));
       console.log(event);
