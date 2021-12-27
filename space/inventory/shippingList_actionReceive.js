da1b241550be7dd71ace66fadf39d19b76480dcb
kintone-mng <89760706+kintone-mng@users.noreply.github.com>
2021-12-27 17:01:11 +0900

    Update shippingList_actionReceive.js

--- space/inventory/shippingList_actionReceive.js
+++ space/inventory/shippingList_actionReceive.js
@@ -13,6 +13,13 @@
     // データ複製ボタン受取
     if(sessionStorage.getItem('is_copy_shipdata')){
       event.record=JSON.parse(sessionStorage.getItem('copy_shipdata'));
+      event.record.Contractor.lookup=true;
+      event.record.instName.lookup=true;
+      event.record.sys_prjId.lookup=true;
+      let devicelistValue=event.record.deviceList.value;
+      for(let i in devicelistValue){
+        devicelistValue[i].value.mNickname.lookup=true;
+      }
       console.log(event);
       sessionStorage.removeItem('is_copy_shipdata');
     }
