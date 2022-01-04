81a0a485482f34ecc8681302eef29e6de91a3663
kintone-mng <89760706+kintone-mng@users.noreply.github.com>
2022-01-04 20:03:30 +0900

    Update shippingList_actionReceive.js

--- space/inventory/shippingList_actionReceive.js
+++ space/inventory/shippingList_actionReceive.js
@@ -1,7 +1,42 @@
 (function () {
   'use strict';
 
+  kintone.events.on('app.record.create.show', function (event) {
+    // event.record.prjNum.disabled = true;
+    //コピー元の「prjNum」の値をsessionStorageの値を代入
+    // event.record.prjNum.value = sessionStorage.getItem('prjNum');
+    // event.record.shipType.value = sessionStorage.getItem('shipType');
+    // event.record.tarDate.value = sessionStorage.getItem('tarDate');
+    // event.record.instName.value = sessionStorage.getItem('instName');
+    // event.record.instName.lookup = true;
+    console.log(sessionStorage.getItem('is_copy_shipdata'));
+    // データ複製ボタン受取
+    if(sessionStorage.getItem('is_copy_shipdata')){
+      event.record=JSON.parse(sessionStorage.getItem('copy_shipdata'));
+      event.record.Contractor.lookup=true;
+      event.record.instName.lookup=true;
+      event.record.sys_prjId.lookup=true;
+      let devicelistValue=event.record.deviceList.value;
+      for(let i in devicelistValue){
+        devicelistValue[i].value.mNickname.lookup=true;
+      }
+      console.log(event);
+      sessionStorage.removeItem('is_copy_shipdata');
+    }
 
+    //キャンセルした時の処理
+    var cancel_btn = document.getElementsByClassName('gaia-ui-actionmenu-cancel');
+    cancel_btn[0].addEventListener('click', function () {
+      window.close();
+    }, false);
+    //反映したあとはsessionStorageの中身を削除
+    sessionStorage.removeItem('prjNum');
+    sessionStorage.removeItem('shipType');
+    sessionStorage.removeItem('tarDate');
+    sessionStorage.removeItem('instName');
+    sessionStorage.removeItem('copy_shipdata');
+    return event;
+  });
   // kintone.events.on('app.record.create.submit.success', function(event){
   //   sessionStorage.removeItem('is_copy_shipdata');
   //   return event;
