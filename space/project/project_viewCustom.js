e6ad5de4083fb74784efda987162917011af5f69
kintone-mng <89760706+kintone-mng@users.noreply.github.com>
2022-01-04 11:38:25 +0900

    Update project_viewCustom.js

--- space/project/project_viewCustom.js
+++ space/project/project_viewCustom.js
@@ -484,10 +484,11 @@
           krtSetting();
           $('#krtSetBtn').on('click', function () {
             var eRecord = kintone.app.record.get();
-            var krtLength = $('.length').val();
-            var krtOpenType = $('input[name=openType]:checked').val();
-            var krtMethodType = $('input[name=methodType]:checked').val();
-            eRecord.record.deviceList.value[i].value.shipRemarks.value = `WFP\nカーテンレール全長(mm)：${krtLength}\n開き勝手：${krtOpenType}\n取り付け方法：${krtMethodType}`;
+            let krtLength = $('.length').val();
+            let krtOpenType = $('input[name=openType]:checked').val();
+            let krtOpenDetail = $("select[name='openDetail']").val();
+            let krtMethodType = $('input[name=methodType]:checked').val();
+            eRecord.record.deviceList.value[i].value.shipRemarks.value = `WFP\nカーテンレール全長(mm)：${krtLength}\n開き勝手：${krtOpenType}\n開き方向：${krtOpenDetail}\n取り付け方法：${krtMethodType}`;
             kintone.app.record.set(eRecord);
             $('#mwFrame').fadeOut(1000, function () {
               $('#mwFrame').remove();
