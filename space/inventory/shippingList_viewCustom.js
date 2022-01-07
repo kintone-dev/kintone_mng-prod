9be79b79a456ba6a617b6ef11a3ee7139c33a703
kintone-mng <89760706+kintone-mng@users.noreply.github.com>
2022-01-07 14:03:22 +0900

    Update shippingList_viewCustom.js

--- space/inventory/shippingList_viewCustom.js
+++ space/inventory/shippingList_viewCustom.js
@@ -107,6 +107,7 @@
     if(sessionStorage.getItem('is_copy_shipdata')){
       let ssRecord=JSON.parse(sessionStorage.getItem('copy_prjdata'));
       for(let i in ssRecord){
+        console.log(ssRecord[i].fcode)
         console.log(event.record[ssRecord[i].fcode]);
         console.log(event.record[ssRecord[i].fcode].value);
         event.record[ssRecord[i].fcode].value=ssRecord[i].value;
