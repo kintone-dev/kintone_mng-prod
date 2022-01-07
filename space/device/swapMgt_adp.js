360107cbaecbe16763f418f603f9fa8340e41b60
kintone-mng <89760706+kintone-mng@users.noreply.github.com>
2022-01-07 17:27:42 +0900

    Update swapMgt_adp.js

--- space/device/swapMgt_adp.js
+++ space/device/swapMgt_adp.js
@@ -24,89 +24,92 @@
     }else{
       setBtn_header('submit_swap', '登録');
       $('#submit_swap').on('click', function(){
-        startLoad();
-        // sys_defective_recordID
-        // sys_repaired_recordID
-        // sys_isSubmit
-        let is_putdefective=new Boolean();
-        let is_putrepaired=new Boolean();
-        console.log('is_putdefective: '+is_putdefective);
-        console.log('is_putrepaired: '+is_putrepaired);
-        let get_repairedInfo={
-          'app': sysid.DEV.app_id.sNum,
-          'id': event.record.sys_repaired_recordID.value
-        };
-        kintone.api(kintone.api.url('/k/v1/record.json', true), 'GET', get_repairedInfo).then(function(resp){
-          console.log(resp);
-          if(resp.record.sState.value=='再生品'){
-            // 故障品シリアル情報取得
-            let get_defectiveInfo={
-              'app': sysid.DEV.app_id.sNum,
-              'id': event.record.sys_defective_recordID.value
-            };
-            kintone.api(kintone.api.url('/k/v1/record.json', true), 'GET', get_defectiveInfo).then(async function(resp){
-              console.log(resp);
-              // 故障品シリアルの「状態」を更新
-              let set_defectiveInfo={
+        let put_confirm=window.confirm('現在のデータで登録しても問題ありませんか？');
+        if(put_confirm){
+          startLoad();
+          // sys_defective_recordID
+          // sys_repaired_recordID
+          // sys_isSubmit
+          let is_putdefective=new Boolean();
+          let is_putrepaired=new Boolean();
+          console.log('is_putdefective: '+is_putdefective);
+          console.log('is_putrepaired: '+is_putrepaired);
+          let get_repairedInfo={
+            'app': sysid.DEV.app_id.sNum,
+            'id': event.record.sys_repaired_recordID.value
+          };
+          kintone.api(kintone.api.url('/k/v1/record.json', true), 'GET', get_repairedInfo).then(function(resp){
+            console.log(resp);
+            if(resp.record.sState.value=='再生品'){
+              // 故障品シリアル情報取得
+              let get_defectiveInfo={
                 'app': sysid.DEV.app_id.sNum,
-                'id': event.record.sys_defective_recordID.value,
-                'record': {'sState': {'value': '検証待ち'}}
+                'id': event.record.sys_defective_recordID.value
               };
-              await kintone.api(kintone.api.url('/k/v1/record.json', true), 'PUT', set_defectiveInfo).then(function(resp){
-                is_putdefective=true;
-                console.log('is_putdefective: '+is_putdefective);
-                console.log('is_putrepaired: '+is_putrepaired);
-              });
-
-              // 交換品シリアルに、故障品シリアルからのデータを上書き
-              let set_repairedInfo={
-                'app': sysid.DEV.app_id.sNum,
-                'id': event.record.sys_repaired_recordID.value,
-                'record': {
-                  'instName': {'value': resp.record.instName.value},
-                  'pkgid': {'value': resp.record.pkgid.value},
-                  'warranty_startDate': {'value': resp.record.warranty_startDate.value},
-                  'warranty_period': {'value': resp.record.warranty_period.value},
-                  'use_stopDate': {'value': resp.record.use_stopDate.value},
-                  'use_endDate': {'value': resp.record.use_endDate.value},
-                  'toastcam_bizUserId': {'value': resp.record.toastcam_bizUserId.value},
-                  'orgName': {'value': resp.record.orgName.value},
-                  'receiver': {'value': resp.record.receiver.value},
-                  'churn_type': {'value': resp.record.churn_type.value},
-
-                  'sendDate': {'value': event.record.repaired_sendDate.value}
-                  // 'warranty_endDate': {'value': resp.record.churn_type.value}, //計算式を確立の上、コメント解除する
-                  // 'shipment': {'value': resp.record.churn_type.value}　//運用上の扱いを確立の上、コメント解除する
-                }
-              };
-              await kintone.api(kintone.api.url('/k/v1/record.json', true), 'PUT', set_repairedInfo).then(function(resp){
-                is_putrepaired=true;
-                console.log('is_putdefective: '+is_putdefective);
-                console.log('is_putrepaired: '+is_putrepaired);
-              });
-
-              // 当レコード「sys_isSubmit」値を更新
-              console.log('is_putdefective: '+is_putdefective);
-              console.log('is_putrepaired: '+is_putrepaired);
-              if(is_putdefective && is_putrepaired){
-                alert('登録完了しました');
-                let set_isSubmit={
-                  'app': kintone.app.getId(),
-                  'id': kintone.app.record.getId(),
-                  'record': {'sys_isSubmit': {'value': 'true'}}
+              kintone.api(kintone.api.url('/k/v1/record.json', true), 'GET', get_defectiveInfo).then(async function(resp){
+                console.log(resp);
+                // 故障品シリアルの「状態」を更新
+                let set_defectiveInfo={
+                  'app': sysid.DEV.app_id.sNum,
+                  'id': event.record.sys_defective_recordID.value,
+                  'record': {'sState': {'value': '検証待ち'}}
                 };
-                kintone.api(kintone.api.url('/k/v1/record.json', true), 'PUT', set_isSubmit).then(function(resp){location.reload();});
-              }
-              
-            });
-          }else{
+                await kintone.api(kintone.api.url('/k/v1/record.json', true), 'PUT', set_defectiveInfo).then(function(resp){
+                  is_putdefective=true;
+                  console.log('is_putdefective: '+is_putdefective);
+                  console.log('is_putrepaired: '+is_putrepaired);
+                });
+
+                // 交換品シリアルに、故障品シリアルからのデータを上書き
+                let set_repairedInfo={
+                  'app': sysid.DEV.app_id.sNum,
+                  'id': event.record.sys_repaired_recordID.value,
+                  'record': {
+                    'instName': {'value': resp.record.instName.value},
+                    'pkgid': {'value': resp.record.pkgid.value},
+                    'warranty_startDate': {'value': resp.record.warranty_startDate.value},
+                    'warranty_period': {'value': resp.record.warranty_period.value},
+                    'use_stopDate': {'value': resp.record.use_stopDate.value},
+                    'use_endDate': {'value': resp.record.use_endDate.value},
+                    'toastcam_bizUserId': {'value': resp.record.toastcam_bizUserId.value},
+                    'orgName': {'value': resp.record.orgName.value},
+                    'receiver': {'value': resp.record.receiver.value},
+                    'churn_type': {'value': resp.record.churn_type.value},
+
+                    'sendDate': {'value': event.record.repaired_sendDate.value}
+                    // 'warranty_endDate': {'value': resp.record.churn_type.value}, //計算式を確立の上、コメント解除する
+                    // 'shipment': {'value': resp.record.churn_type.value}　//運用上の扱いを確立の上、コメント解除する
+                  }
+                };
+                await kintone.api(kintone.api.url('/k/v1/record.json', true), 'PUT', set_repairedInfo).then(function(resp){
+                  is_putrepaired=true;
+                  console.log('is_putdefective: '+is_putdefective);
+                  console.log('is_putrepaired: '+is_putrepaired);
+                });
+
+                // 当レコード「sys_isSubmit」値を更新
+                console.log('is_putdefective: '+is_putdefective);
+                console.log('is_putrepaired: '+is_putrepaired);
+                if(is_putdefective && is_putrepaired){
+                  alert('登録完了しました');
+                  let set_isSubmit={
+                    'app': kintone.app.getId(),
+                    'id': kintone.app.record.getId(),
+                    'record': {'sys_isSubmit': {'value': 'true'}}
+                  };
+                  kintone.api(kintone.api.url('/k/v1/record.json', true), 'PUT', set_isSubmit).then(function(resp){location.reload();});
+                }
+                
+              });
+            }else{
+              errorMessage('unknowSN');
+            }
+          }).catch(function(error){
+            console.log(error);
             errorMessage('unknowSN');
-          }
-        }).catch(function(error){
-          console.log(error);
-          errorMessage('unknowSN');
-        });
-        endLoad();
+          });
+          endLoad();
+        }
       });
     }
     
