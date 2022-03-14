bfe3b95d6f8d467c65c4add670aef6b331dc3f6b
kintone-mng <89760706+kintone-mng@users.noreply.github.com>
2022-03-14 19:32:51 +0900

    Create shippingList_viewCTL.js

---
+++ space/inventory/shippingList_viewCTL.js
@@ -0,0 +1,67 @@
+(function(){
+  'use strict';
+  /** イベント　通常イベント発生時 */
+  // 一覧表示
+  kintone.events.on('app.record.index.show', function(event){
+    startLoad();
+    /** 初期設定 start */
+    /** 初期設定 end */
+    endLoad();
+    return event;
+  });
+  // 新規レコード作成
+  kintone.events.on('app.record.create.show', function(event){
+    startLoad();
+    /** 初期設定 start */
+    /** 初期設定 end */
+
+    /** アクション受領時 start */
+    /** アクション受領時 end */
+
+    /** 条件付き設定 start */
+    /** 条件付き設定 end */
+    endLoad();
+    return event;
+  });
+  // レコード編集
+  kintone.events.on('app.record.edit.show', function(event){
+    startLoad();
+    /** 初期設定 start */
+    /** 初期設定 end */
+
+    /** 条件付き設定 start */
+    /** 条件付き設定 end */
+    endLoad();
+    return event;
+  });
+  // レコード詳細閲覧
+  kintone.events.on('app.record.detail.show', function(event){
+    startLoad();
+    /** 初期設定 start */
+    /** 初期設定 end */
+
+    /** 条件付き設定 start */
+    /** 条件付き設定 end */
+    endLoad();
+    return event;
+  });
+
+  /** イベント　プロセス進行 */
+  kintone.events.on('app.record.detail.process.proceed', function (event) {
+    startLoad();
+    // 
+    endLoad();
+    return event;
+  });
+
+  /** イベント 項目変更 */
+  // 
+  kintone.events.on('app.record.create.chante.', function(event){
+    startLoad();
+    // 
+    endLoad();
+    return event;
+  });
+
+  /** 実行関数 */
+})();
