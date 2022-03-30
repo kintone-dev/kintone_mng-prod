c03eb227b0511e629315cff00db4e021f1d3553a
kintone-mng <89760706+kintone-mng@users.noreply.github.com>
2022-03-30 11:13:02 +0900

    Update shippingList_viewCTL.js

--- space/inventory/shippingList_viewCTL.js
+++ space/inventory/shippingList_viewCTL.js
@@ -187,7 +187,9 @@
 
   function temp_fDesibale(event){
     let get_fCode = getFields();
+    console.log(get_fCode);
     for(let i=8; i<get_fCode.length; i++){
+      console.log(event.record[get_fCode[i].var]);
       event.record[get_fCode[i].var].disabled = false;
     }
     return event;
