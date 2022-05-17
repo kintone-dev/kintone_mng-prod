1886b6d0ec6abd5db38e692583350799265917aa
3sssk-maeda <k-maeda@3sss.co.jp>
2022-05-17 10:36:46 +0900

    Update cancel_linkageCTL.js

--- space/atlasSS/cancel_linkageCTL.js
+++ space/atlasSS/cancel_linkageCTL.js
@@ -1,6 +1,8 @@
 (function() {
   'use strict';
   kintone.events.on('app.record.detail.show', function(event) {
+    setFieldShown('sys_sn_recordId', false);
+
     var linkageBtn = setBtn('btn_linkage_sNum', 'シリアル管理連携');
     $('#' + linkageBtn.id).on('click', async function () {
       startLoad();
