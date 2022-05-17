c78bf5602ef09b47dd5ab3a41f017ac600fcf426
3sssk-maeda <k-maeda@3sss.co.jp>
2022-05-17 12:16:52 +0900

    Update cancel_linkageCTL.js

--- space/atlasSS/cancel_linkageCTL.js
+++ space/atlasSS/cancel_linkageCTL.js
@@ -6,10 +6,11 @@
       startLoad();
       // エラー処理
 
-      // 更新用json作成
+      /* ＞＞＞ 更新用json作成 ＜＜＜ */
       let updateBody={app:sysid.DEV.app_id.sNum, records:[]}
 
       for(const device of event.record.device_info.value){
+        console.log(setShiptype[device.value.sState.value]);
         if(!setShiptype[device.value.sState.value]){
           let set_updateRecord={
             id: device.value.sys_sn_recordId.value,
@@ -21,13 +22,14 @@
         }
       }
 
-      console.log(updateBody);
-
-      // シリアル管理連携
+      /* ＞＞＞ シリアル管理連携 ＜＜＜ */
       let response_PUT={};
-      if(updateBody.records.length>0) response_PUT = await kintone.api(kintone.api.url('/k/v1/records.json', true), 'PUT', updateBody);
+      console.log(updateBody.records.length);
+      if(updateBody.records.length>0){
+        response_PUT = await kintone.api(kintone.api.url('/k/v1/records.json', true), 'PUT', updateBody);
+      }
 
-      // ログ作成
+      /* ＞＞＞ ログ作成 ＜＜＜ */
       let logUpdateBody={app:sysid.ASS2.app_id.cancellation, records:[]};
 
       response_PUT.then(async function (resp) {
