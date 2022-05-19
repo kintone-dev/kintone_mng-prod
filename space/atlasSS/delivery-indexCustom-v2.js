8c5b8b93560229d6fe2207f04a0082c470c65042
3sssk-maeda <k-maeda@3sss.co.jp>
2022-05-19 17:23:13 +0900

    Update delivery-indexCustom-v2.js

--- space/atlasSS/delivery-indexCustom-v2.js
+++ space/atlasSS/delivery-indexCustom-v2.js
@@ -15,73 +15,95 @@
         会員情報に情報を更新
         AL専用を会員情報登録済に
       */
-      var getNewMemBody = {
+      // 登録する会員情報を検索
+      let getNewMemBody = {
         'app': kintone.app.getId(),
         'query': 'working_status in ("準備中") and application_type in ("新規申込") and syncStatus_member not in ("success")'
       };
-      var newMemData = await kintone.api(kintone.api.url('/k/v1/records.json', true), 'GET', getNewMemBody)
+      let newMemData = await kintone.api(kintone.api.url('/k/v1/records.json', true), 'GET', getNewMemBody)
         .then(function (resp) {
           return resp;
         }).catch(function (error) {
           return error;
         });
-      console.log(newMemData);
-
       var newMemList = newMemData.records;
       if(newMemList.length==0){
-        console.log('連携する会員データがありません');
+        alert('連携する会員データがありません');
       }else{
-        //新規申込データ作成
-        var postMemData = [];
-        //ステータス,ログデータ作成
-        var putWStatNewData = [];
+        // 会員情報が重複していないか確認
+        var memIdArray = [];
+        for (const newMem of newMemList) {
+          memIdArray.push('"' + newMem.member_id.value + '"');
+        }
+        let getMemBody = {
+          'app': sysid.ASS2.app_id.member,
+          'query': 'member_id in (' + memIdArray.join() + ')'
+        };
+        let memData = await kintone.api(kintone.api.url('/k/v1/records.json', true), 'GET', getMemBody)
+        .then(function (resp) {
+          return resp;
+        }).catch(function (error) {
+          return error;
+        });
+
+        // 新規申込データ作成
+        let postMemData=[];
+        // ステータス,ログデータ作成
+        let putWStatNewData=[];
+        // 重複データ配列
+        let dupeData=[];
         //新規申込内容作成
-        for(let i in newMemList) {
-          var postBody_member = {
-            'member_id': {
-              'value': newMemList[i].member_id.value
-            },
-            'member_type': {
-              'value': newMemList[i].member_type.value
-            },
-            'application_datetime': {
-              'value': newMemList[i].application_datetime.value
-            },
-            'start_date': {
-              'value': newMemList[i].start_date.value
-            },
-            'application_type': {
-              'value': newMemList[i].application_type.value
-            }
-          };
-          var putBody_workStatNew = {
-            'id': newMemList[i].レコード番号.value,
-            'record': {
-              syncStatus_member: {},
-              syncLog_list: {
-                value: [{
-                  value:{
-                    // ログ更新時間（サーバーから時間を取得）
-                    syncLog_date: {value: forListDate()},
-                    // 実施内容
-                    syncLog_type: {value: 'KT-会員情報'},
-                    // 成功判断
-                    syncLog_status: {},
-                    // ログメッセージ（レスポンス内容）
-                    syncLog_message: {},
+        for(const newMem of newMemList) {
+          for(const mem of memData.records){
+            if(newMem.member_id.value==mem.member_id.value){
+              dupeData.push(newMem.member_id.value)
+            }else{
+              var postBody_member = {
+                'member_id': {
+                  'value': newMem.member_id.value
+                },
+                'member_type': {
+                  'value': newMem.member_type.value
+                },
+                'application_datetime': {
+                  'value': newMem.application_datetime.value
+                },
+                'start_date': {
+                  'value': newMem.start_date.value
+                },
+                'application_type': {
+                  'value': newMem.application_type.value
+                }
+              };
+              var putBody_workStatNew = {
+                'id': newMem.レコード番号.value,
+                'record': {
+                  syncStatus_member: {},
+                  syncLog_list: {
+                    value: [{
+                      value:{
+                        // ログ更新時間（サーバーから時間を取得）
+                        syncLog_date: {value: forListDate()},
+                        // 実施内容
+                        syncLog_type: {value: 'KT-会員情報'},
+                        // 成功判断
+                        syncLog_status: {},
+                        // ログメッセージ（レスポンス内容）
+                        syncLog_message: {},
+                      }
+                    }]
                   }
-                }]
-              }
+                }
+              };
+              postMemData.push(postBody_member);
+              putWStatNewData.push(putBody_workStatNew);
             }
-          };
-          postMemData.push(postBody_member);
-          putWStatNewData.push(putBody_workStatNew);
+          }
         }
 
         try {
-          if((postMemData.length>0)) await postRecords(sysid.ASS2.app_id.member, postMemData)
+          if(postMemData.length>0) await postRecords(sysid.ASS2.app_id.member, postMemData)
             .then(async function (resp) {
-              console.log(resp);
               alert('新規申込情報連携に成功しました。');
               // ステータス,ログ更新
               for(const stat of putWStatNewData){
@@ -94,7 +116,6 @@
               await putRecords(kintone.app.getId(), putWStatNewData)
             }).catch(async function (error) {
               alert('新規申込情報連携に失敗しました。システム管理者に連絡してください。');
-              console.log(error);
               // エラーステータス更新
               for(const stat of putWStatNewData){
                 stat.record.syncStatus_member.value = 'error';
@@ -109,6 +130,8 @@
           alert('新規申込情報連携に失敗しました。システム管理者に連絡してください。');
         }
       }
+
+      if(dupeData.length>0) alert("次の契約IDは登録済みです"+dupeData)
 
       endLoad();
       return event;
