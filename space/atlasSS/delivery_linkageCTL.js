2d9cbc6c4a25708bbc3e26ce2831c5e8876b6edc
3sssk-maeda <k-maeda@3sss.co.jp>
2022-05-20 12:33:31 +0900

    Update delivery_linkageCTL.js

--- space/atlasSS/delivery_linkageCTL.js
+++ space/atlasSS/delivery_linkageCTL.js
@@ -1,10 +1,16 @@
 (function() {
   'use strict';
   kintone.events.on(['app.record.create.submit.success','app.record.edit.submit.success'], function(event) {
-    console.log(event.record);
-    // if(){
+    if(!event.record.working_status.value=='出荷完了'){
+      return event;
+    }
+    if(event.record.syncStatus_batch.value=='error'){
+      return {result: false, error: {target: 'syncStatus_batch', code: 'ass_emptyvalue'}};
+    }
+    if(!event.record.syncStatus_serial.value=='success'){
+      console.log(event.record.syncStatus_serial.value);
+    }
 
-    // }
 
     return event;
   });
