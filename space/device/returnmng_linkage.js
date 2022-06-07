631870937dd3c0ee04caf59092093a3fea6d4d54
3sssk-maeda <k-maeda@3sss.co.jp>
2022-06-07 12:05:00 +0900

    Update returnmng_linkage.js

--- space/device/returnmng_linkage.js
+++ space/device/returnmng_linkage.js
@@ -3,6 +3,7 @@
   kintone.events.on('app.record.edit.show',async function(event) {
     var linkage_return=setBtn('btn_linkage_return','シリアルテーブル作成');
     $('#'+linkage_return.id).on('click', function(){
+      var eRecord = kintone.app.record.get();
       let snArray = (eRecord.record.sNums.value).split(/\r\n|\n/);
       console.log(snArray);
 			// snArray.forEach(function(snum){
