ed7d6c1615f327e9b47a06740694657da414cf97
3sssk-maeda <k-maeda@3sss.co.jp>
2022-06-07 12:23:03 +0900

    Update returnmng_linkage.js

--- space/device/returnmng_linkage.js
+++ space/device/returnmng_linkage.js
@@ -4,8 +4,13 @@
   kintone.events.on('app.record.edit.show',async function(event) {
     var linkage_return=setBtn('btn_linkage_return','シリアルテーブル作成');
     $('#'+linkage_return.id).on('click', function(){
+      startLoad();
       var eRecord = kintone.app.record.get();
-      createDeviceList(eRecord)
+      let createDLResult = createDeviceList(eRecord)
+      if(!createDLResult.result){
+        endLoad();
+        return event;
+      }
 
     });
 
@@ -17,6 +22,7 @@ function createDeviceList(eRecord){
   let snArray = (eRecord.record.sNums.value).split(/\r\n|\n/);
   for(const snums of snArray){
     if(!snums || !snums.match(/\S/g)){
+      alert('空白文字か空の改行が含まれています')
       return {result: false, error: {target: 'createDeviceList', code: 'createDeviceList_snumEmpty'}};
     }
   }
