1df85742e8de683f851de07b7dbf294f99084749
kintone-mng <89760706+kintone-mng@users.noreply.github.com>
2022-06-16 19:08:15 +0900

    Update cancel_viewCustom.js

--- space/atlasSS/cancel_viewCustom.js
+++ space/atlasSS/cancel_viewCustom.js
@@ -33,29 +33,27 @@
   kintone.events.on('app.record.create.submit', async function(event){
     const memberId = event.record.member_id.value;
     const applicationType = '新規申込';
-    const get_appCampaign = await (kintone.api(kintone.api.url('/k/v1/records.json', true), 'GET', {
+    const get_appCampaign = (await kintone.api(kintone.api.url('/k/v1/records.json', true), 'GET', {
       app: sysid.ASS2.app_id.shipment,
       query: 'member_id = "' + memberId + '" and application_type in ("' + applicationType + '")',
       fields: ['appCampaign']
-    }));
+    })).records;
 
-    console.log(get_appCampaign.records);
-
-    // if(get_appCampaign == undefined){
-    //   event.record.member_id.error = '不明なエラー';
-    //   event.error = 'レコードを保存できませんでした。エラー内容をご確認ください。';
-    // }
-    // else if(get_appCampaign.length > 1){
-    //   event.record.member_id.error = '同じ契約IDに申し込み種別が新規申込になっているデータが複数存在します。';
-    //   event.error = 'レコードを保存できませんでした。エラー内容をご確認ください。';
-    // }
-    // else if(get_appCampaign.length < 1){
-    //   event.record.member_id.error = '申し込み種別が新規申込になっている契約IDが見つかりませんでした。';
-    //   event.error = 'レコードを保存できませんでした。エラー内容をご確認ください。';
-    // }
-    // else{
-    //   event.record.appCampaign.value = 'ttestt';
-    // }
+    if(get_appCampaign == undefined){
+      event.record.member_id.error = '不明なエラー';
+      event.error = 'レコードを保存できませんでした。エラー内容をご確認ください。';
+    }
+    else if(get_appCampaign.length > 1){
+      event.record.member_id.error = '同じ契約IDに申し込み種別が新規申込になっているデータが複数存在します。';
+      event.error = 'レコードを保存できませんでした。エラー内容をご確認ください。';
+    }
+    else if(get_appCampaign.length < 1){
+      event.record.member_id.error = '申し込み種別が新規申込になっている契約IDが見つかりませんでした。';
+      event.error = 'レコードを保存できませんでした。エラー内容をご確認ください。';
+    }
+    else{
+      event.record.appCampaign.value = get_appCampaign[0].appCampaign.value;
+    }
     return false;
   });
 
