5686da4077799f1bbd72e04475d786a6f56d1012
3sssk-maeda <k-maeda@3sss.co.jp>
2022-06-16 13:05:02 +0900

    Update cancel_linkageCTL.js

--- space/atlasSS/cancel_linkageCTL.js
+++ space/atlasSS/cancel_linkageCTL.js
@@ -187,17 +187,7 @@ async function returnCheck(event){
   let notReturnArray = [];
   for(const deviceList of event.record.device_info.value){
     if(deviceList.value.sState.value=='返却待ち'){
-      let returnItem = {
-        value:{
-          sState:{
-            value: '返却待ち'
-          },
-          device_serial_number:{
-            value: deviceList.value.device_serial_number.value
-          }
-        }
-      }
-      returnArray.push(returnItem)
+      returnArray.push(deviceList)
     } else {
       notReturnArray.push(deviceList)
     }
