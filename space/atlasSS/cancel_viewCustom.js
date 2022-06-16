f4d92f47a4704d667f179150a5d432acd2afdbdf
kintone-mng <89760706+kintone-mng@users.noreply.github.com>
2022-06-16 18:46:06 +0900

    Update cancel_viewCustom.js

--- space/atlasSS/cancel_viewCustom.js
+++ space/atlasSS/cancel_viewCustom.js
@@ -41,21 +41,21 @@
 
     console.log(get_appCampaign);
 
-    if(get_appCampaign == undefined){
-      event.record.member_id.error = '不明なエラー';
-      event.error = 'レコードを保存できませんでした。エラー内容をご確認ください。';
-    }
-    else if(get_appCampaign.length > 1){
-      event.record.member_id.error = '同じ契約IDに申し込み種別が新規申込になっているデータが複数存在します。';
-      event.error = 'レコードを保存できませんでした。エラー内容をご確認ください。';
-    }
-    else if(get_appCampaign.length < 1){
-      event.record.member_id.error = '申し込み種別が新規申込になっている契約IDが見つかりませんでした。';
-      event.error = 'レコードを保存できませんでした。エラー内容をご確認ください。';
-    }
-    else{
-      event.record.appCampaign.value = 'ttestt';
-    }
+    // if(get_appCampaign == undefined){
+    //   event.record.member_id.error = '不明なエラー';
+    //   event.error = 'レコードを保存できませんでした。エラー内容をご確認ください。';
+    // }
+    // else if(get_appCampaign.length > 1){
+    //   event.record.member_id.error = '同じ契約IDに申し込み種別が新規申込になっているデータが複数存在します。';
+    //   event.error = 'レコードを保存できませんでした。エラー内容をご確認ください。';
+    // }
+    // else if(get_appCampaign.length < 1){
+    //   event.record.member_id.error = '申し込み種別が新規申込になっている契約IDが見つかりませんでした。';
+    //   event.error = 'レコードを保存できませんでした。エラー内容をご確認ください。';
+    // }
+    // else{
+    //   event.record.appCampaign.value = 'ttestt';
+    // }
     return event;
   });
 
