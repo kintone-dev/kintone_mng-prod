01b9b2f9a0b66210294f20a208babe06dab33132
3sssk-maeda <k-maeda@3sss.co.jp>
2022-06-16 14:32:56 +0900

    Update cancel_linkageCTL.js

--- space/atlasSS/cancel_linkageCTL.js
+++ space/atlasSS/cancel_linkageCTL.js
@@ -302,11 +302,10 @@ async function reportLink(event){
     console.log('レポートの取得に失敗しました');
     return reportData;
   }
-  if(reportData.resp.length==0){
+  if(reportData.resp.records.length==0){
     console.log('該当するレポートがありません');
     return {result: false, error:  {target: 'reportLink', code: 'reportLink_noData'}};
   }
-  console.log(reportData);
   // レポート在庫連携用json作成
   let reportStockJson = {
     app: sysid.INV.app_id.report,
