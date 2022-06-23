c5ed79aff19199e0820a3bf6a38357fc5284dbe6
kintone-mng <89760706+kintone-mng@users.noreply.github.com>
2022-06-23 20:16:21 +0900

    Update shippingList_viewCTL-v2.js

--- space/inventory/shippingList_viewCTL-v2.js
+++ space/inventory/shippingList_viewCTL-v2.js
@@ -212,7 +212,6 @@
       delete spliceRecord['作業者'];
       delete spliceRecord['更新日時'];
       delete spliceRecord['更新者'];
-      delete spliceRecord.deviceList;
       spliceRecord.recordSplitType.value = ['分岐'];
 
       let NewShippingListBody = {
