6fa220da349789d9de2844664100bc821596ad2e
kintone-mng <89760706+kintone-mng@users.noreply.github.com>
2022-08-05 13:11:06 +0900

    Update deviceAccount_viewCustom.js

--- space/device/deviceAccount_viewCustom.js
+++ space/device/deviceAccount_viewCustom.js
@@ -1,60 +1,56 @@
 (function () {
   'use strict';
 
-  // var events_ced = [
-  //   'mobile.app.record.create.show',
-  //   'mobile.app.record.edit.show',
-  //   'mobile.app.record.detail.show',
-  //   'app.record.create.show',
-  //   'app.record.detail.show',
-  //   'app.record.edit.show'
-  // ];
+  var events_ced = [
+    'mobile.app.record.create.show',
+    'mobile.app.record.edit.show',
+    'mobile.app.record.detail.show',
+    'app.record.create.show',
+    'app.record.detail.show',
+    'app.record.edit.show'
+  ];
 
-  // kintone.events.on(events_ced, function (event) {
-  //   event.record.toastcam_bizUserId.disabled = true;
-  //   event.record.toastcam_bizUserPassword.disabled = true;
+  kintone.events.on(events_ced, function (event) {
+    event.record.toastcam_bizUserId.disabled = true;
+    event.record.toastcam_bizUserPassword.disabled = true;
 
-  //   return event;
-  // });
+    return event;
+  });
 
-  // kintone.events.on('app.record.create.show', function (event) {
-  //   $.ajax({
-  //     type: 'GET'
-  //   }).done(function (data, status, xhr) {
-  //     var serverDate = new Date(xhr.getResponseHeader('Date')).getTime(); //サーバー時刻を代入
-  //     var utcNum = Math.floor(serverDate / 5000); //5秒の幅を持って、切り上げる
+  kintone.events.on('app.record.create.change.cmsType', function (event) {
+    const cmstype = event.record.cmsType.value;
+    if(cmstype == 'TCアカウント'){
+      $.ajax({
+        type: 'GET'
+      }).done(function (data, status, xhr) {
+        var serverDate = new Date(xhr.getResponseHeader('Date')).getTime(); //サーバー時刻を代入
+        var utcNum = Math.floor(serverDate / 5000); //5秒の幅を持って、切り上げる
 
-  //     var eRecord = kintone.app.record.get(); //レコード値を取得
+        var eRecord = kintone.app.record.get(); //レコード値を取得
 
-  //     eRecord.record.toastcam_bizUserId.value = 'TC_' + utcNum + '@accel-lab.com';
-  //     eRecord.record.toastcam_bizUserPassword.value = pw_generator(10);
-  //     kintone.app.record.set(eRecord); //変更内容を反映
-  //   });
+        eRecord.record.cmsID.value = 'TC_' + utcNum + '@accel-lab.com';
+        eRecord.record.cmsPW.value = pw_generator(10);
+        // eRecord.record.contractStatus.value = '';
+        // eRecord.record.usageStatus.value = '';
+        kintone.app.record.set(eRecord); //変更内容を反映
+      });
+    }
+    if(cmstype == 'Danaアカウント'){
+      event.record.cmsID.value = '';
+      event.record.cmsPW.value = '';
+    }
 
-  //   return event;
-  // });
+    return event;
+  });
 
-  // kintone.events.on(['app.record.detail.show', 'app.record.edit.show'], function (event) {
-  //   var openTcms = setBtn('btn_open_tCMS', 'CMS');
+  kintone.events.on(['app.record.detail.show', 'app.record.edit.show'], function (event) {
+    var openTcms = setBtn('btn_open_tCMS', 'ToastComCMS');
     
-  //   $('#' + openTcms.id).on('click', function () {
-  //     window.open('https://cms.toastcam.com/#/bizUserManagementWrite?searchType=&searchWord=&pageNumber=1&count=10&time=1629776725393', '_blank');
-  //   });
+    $('#' + openTcms.id).on('click', function () {
+      window.open('https://cms.toastcam.com/#/bizUserManagementWrite?searchType=&searchWord=&pageNumber=1&count=10&time=1629776725393', '_blank');
+    });
 
-  //   return event;
-  // });
-  
-  // 保存前チェック
-  // 設置先がass-ASSの場合、PKG-ID/部屋・物件名/会員ID
-  // kintone.events.on(['app.record.create.submit','app.record.edit.submit'], function(event){
-  //   if(event.record.instName.value=='ass-ASS' && event.record.pkgid.value==undefined){
-  //     event.error=('Titanの会員IDを入力してください');
-  //   }
-  //   return event;
-  // });
+    return event;
+  });
 
-  kintone.events.on('app.record.index.show', function(event){
-    console.log('hello');
-    setAccount();
-  })
 })();
