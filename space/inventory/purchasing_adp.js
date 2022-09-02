de60c530e2220bec745e34298a6bc61fd473e9d7
kintone-mng <89760706+kintone-mng@users.noreply.github.com>
2022-09-02 12:07:42 +0900

    Update purchasing_adp.js

--- space/inventory/purchasing_adp.js
+++ space/inventory/purchasing_adp.js
@@ -7,40 +7,40 @@
     var sendDate = event.record.arrivalDate.value;
     sendDate = sendDate.replace(/-/g, '');
     sendDate = sendDate.slice(0, -2);
-    var reportData = await checkEoMReport(sendDate, kintone.getLoginUser());
-    if(Array.isArray(reportData)){
-      if (reportData[0] == 'false') {
-        event.error = '対応した日付のレポートは' + reportData[1] + '済みです。';
-        endLoad();
-        return event;
-      } else if(reportData[0] == 'true'){
-        if(!confirm('対応した日付のレポートは' + reportData[1] + '済みです。\n作業を続けますか？')){
-          endLoad();
-          return event;
-        }
-      }
-    }
-    if (nStatus === '仕入完了') {
-      var devArray = [];
-      for (let i in event.record.arrivalList.value) {
-        devArray.push(event.record.arrivalList.value[i].value.mCode.value);
-      }
-      console.log(devArray);
-      //重複チェック関数
-      function existsSameValue(a) {
-        var s = new Set(a);
-        return s.size != a.length;
-      }
-      if (existsSameValue(devArray)) {
-        event.error = '品目が重複しています'
-        endLoad();
-        return event;
-      }
-      // 在庫処理
-      await stockCtrl(event, kintone.app.getId());
-      // レポート処理
-      await reportCtrl(event, kintone.app.getId());
-    }
+    // var reportData = await checkEoMReport(sendDate, kintone.getLoginUser());
+    // if(Array.isArray(reportData)){
+    //   if (reportData[0] == 'false') {
+    //     event.error = '対応した日付のレポートは' + reportData[1] + '済みです。';
+    //     endLoad();
+    //     return event;
+    //   } else if(reportData[0] == 'true'){
+    //     if(!confirm('対応した日付のレポートは' + reportData[1] + '済みです。\n作業を続けますか？')){
+    //       endLoad();
+    //       return event;
+    //     }
+    //   }
+    // }
+    // if (nStatus === '仕入完了') {
+    //   var devArray = [];
+    //   for (let i in event.record.arrivalList.value) {
+    //     devArray.push(event.record.arrivalList.value[i].value.mCode.value);
+    //   }
+    //   console.log(devArray);
+    //   //重複チェック関数
+    //   function existsSameValue(a) {
+    //     var s = new Set(a);
+    //     return s.size != a.length;
+    //   }
+    //   if (existsSameValue(devArray)) {
+    //     event.error = '品目が重複しています'
+    //     endLoad();
+    //     return event;
+    //   }
+    //   // 在庫処理
+    //   await stockCtrl(event, kintone.app.getId());
+    //   // レポート処理
+    //   await reportCtrl(event, kintone.app.getId());
+    // }
     endLoad();
     return event;
   });
