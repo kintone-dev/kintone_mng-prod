fd7951dd96f045cc01e5ffc930fcbbc43ea82993
kintone-mng <89760706+kintone-mng@users.noreply.github.com>
2022-11-10 17:08:09 +0900

    Update caseEvaluation_al_viewCustom.js

--- space/support/caseEvaluation_al_viewCustom.js
+++ space/support/caseEvaluation_al_viewCustom.js
@@ -2,9 +2,36 @@
   'use strict';
   kintone.events.on(['app.record.detail.show', 'app.record.edit.show'], async function(event){
     // btn_renew
+    // sys_supNum
+    // supNum
     let renew=setBtn('btn_renew','更新');
-    renew.onclick=function(){
-      alert('tttt');
+    renew.onclick=async function(){
+      let supNumValue = event.record.supNum.value;
+      let sys_supNumValue = event.record.sys_supNum.value;
+
+      let renewBody = {};
+      if(!supNumValue || supNumValue == sys_supNumValue){
+        renewBody = {
+          app: kintone.app.getId(),
+          id: kintone.app.record.getId(),
+          record: {
+            supNumValue: {value: sys_supNumValue},
+            infoError: {value: []},
+            ErrorMessage: {value: ''}
+          }
+        };
+      }else{
+        renewBody = {
+          app: kintone.app.getId(),
+          id: kintone.app.record.getId(),
+          record: {
+            infoError: {value: ['InformationError']},
+            ErrorMessage: {value: '格納した問い合わせ番号と、連携した問い合わせ番号が一致しません。'}
+          }
+        };
+      }
+      let renewResult = await kintone.api(kintone.api.url('/k/v1/record', true), 'PUT', renewBody);
+      console.log(renewResult);
     }
   });
 })();
