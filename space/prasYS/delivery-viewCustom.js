525b11013b3af9bc4cdec9870462e1c41259dcd5
3sssk-maeda <k-maeda@3sss.co.jp>
2023-02-16 10:03:11 +0900

    devファイル追加

---
+++ space/prasYS/delivery-viewCustom.js
@@ -0,0 +1,118 @@
+(function () {
+  'use strict';
+  // 入力確認
+  kintone.events.on(['app.record.edit.show','app.record.edit.submit'], async function(event){
+    const memberId = event.record.member_id.value;
+    const tar_applicationType = '新規申込';
+    const applicationType = event.record.application_type.value;
+    /**
+     * 新規申込の契約ID重複
+     * 追加申込の契約ID該当なし
+     */
+    const get_appCampaign = (await kintone.api(kintone.api.url('/k/v1/records.json', true), 'GET', {
+      app: sysid.prasYS.app_id.shipment,
+      query: 'member_id = "' + memberId + '" and application_type in ("' + tar_applicationType + '") and $id != "' + kintone.app.record.getId() + '"',
+      fields: ['member_id']
+    })).records;
+    if(get_appCampaign == undefined){
+      event.record.member_id.error = '不明なエラー';
+      event.error = 'レコードを保存できませんでした。エラー内容をご確認ください。';
+    }else if(applicationType == tar_applicationType && get_appCampaign.length > 0){
+      event.record.member_id.error = '契約ID: ' + get_appCampaign[0].member_id.value + '\n同じ契約IDで、申し込み種別が新規申込になっているデータが既に存在します。';
+      event.error = 'レコードを保存できませんでした。エラー内容をご確認ください。';
+    }else if(applicationType !== tar_applicationType && get_appCampaign.length < 1){
+      event.record.member_id.error = '契約ID: ' + memberId + '\n存在しない契約IDです。';
+      event.error = 'レコードを保存できませんでした。エラー内容をご確認ください。';
+    }
+    return event;
+  });
+
+
+
+  var events_aType_show = [
+    'app.record.detail.show',
+    'app.record.create.show',
+    'app.record.edit.show'
+  ];
+  kintone.events.on(events_aType_show, function (event) {
+    var aType = event.record.application_type.value;
+    if (aType.match(/故障交換/)) {
+      // setFieldShown('deviceList', false);
+      setFieldShown('failure_sNum', true);
+      setFieldShown('replacement_sNum', true);
+      setFieldShown('failure_sNum_info', true);
+    } else {
+      // setFieldShown('deviceList', true);
+      setFieldShown('failure_sNum', false);
+      setFieldShown('replacement_sNum', false);
+      setFieldShown('failure_sNum_info', false);
+    }
+    return event;
+  });
+  kintone.events.on('app.record.create.change.application_type', function (event) {
+    var aType = event.record.application_type.value;
+    if (aType.match(/故障交換/)) {
+      // setFieldShown('deviceList', false);
+      setFieldShown('failure_sNum', true);
+      setFieldShown('replacement_sNum', true);
+      setFieldShown('failure_sNum_info', true);
+    } else {
+      // setFieldShown('deviceList', true);
+      setFieldShown('failure_sNum', false);
+      setFieldShown('replacement_sNum', false);
+      setFieldShown('failure_sNum_info', false);
+    }
+    return event;
+  });
+  kintone.events.on(['app.record.create.submit','app.record.edit.submit'], function(event){
+    let ship_deviceList=event.record.deviceList.value;
+    for(let i in ship_deviceList){
+      if(ship_deviceList[i].value.mCode.value=='TC-UB12F-M'){
+        // let SNsQuery=sNumRecords(ship_deviceList[i].value.sNum.value, 'text').SNs.join('","');
+        let SNsQuery=ship_deviceList[i].value.sNum.value;
+        let get_Mac={
+          'app': sysid.DEV.app_id.sNum,
+          'query':'sNum in ("'+SNsQuery+'")'
+        }
+        return kintone.api(kintone.api.url('/k/v1/records.json', true), 'GET', get_Mac).then(function(resp){
+          console.log(resp);
+          for(let y in resp.records){
+            let remarks=ship_deviceList[i].value.shipRemarks.value;
+            if(y==0){
+              if(remarks==undefined) ship_deviceList[i].value.shipRemarks.value='\n＝＝＝MAC address＝＝＝\n'+resp.records[y].macaddress.value;
+              else ship_deviceList[i].value.shipRemarks.value+='\n＝＝＝MAC address＝＝＝\n'+resp.records[y].macaddress.value;
+            }else ship_deviceList[i].value.shipRemarks.value+='\n'+resp.records[y].macaddress.value;
+          }
+          console.log(event.record.deviceList.value);
+          return event;
+        }).catch(function(error){
+          console.log(error);
+        });
+      }
+    }
+    return event;
+  });
+  kintone.events.on('app.record.edit.submit', function(event){
+    const workingstatus = event.record.working_status.value;
+    if(workingstatus == '出荷完了'){
+      const syncstatusbatch = event.record.syncStatus_batch.value;
+      const shipnumber = event.record.ship_number.value;
+      const shippingdatetime = event.record.shipping_datetime.value;
+      console.log(syncstatusbatch);
+      console.log(shipnumber);
+      console.log(shippingdatetime);
+      if(syncstatusbatch == 'error' || syncstatusbatch == '' || !syncstatusbatch){
+        event.error = 'デバイス登録処理に問題があります。\nレコードを保存できませんでした。エラー内容をご確認ください。';
+      }
+      if(!shipnumber){
+        event.record.ship_number.error = '伝票番号が空欄です。';
+        event.error = 'レコードを保存できませんでした。エラー内容をご確認ください。';
+      }
+      if(!shippingdatetime){
+        event.record.shipping_datetime.error = '出荷日時が空欄です。';
+        event.error = 'レコードを保存できませんでした。エラー内容をご確認ください。';
+      }
+    }
+    return event;
+  });
+})();
---
+++ space/prasYS/delivery_linkageCTL-dev.js
@@ -0,0 +1,323 @@
+(function() {
+  'use strict';
+
+  kintone.events.on('app.record.edit.submit', async function(event) {
+    startLoad();
+    // 状態確認
+    let checkStatResult = checkStat(
+      event.record.working_status.value,
+      event.record.syncStatus_batch.value,
+      event.record.application_type.value
+    );
+    // 状態が例外だった場合処理を中止
+    if(!checkStatResult.result){
+      console.log(checkStatResult.error.code);
+      endLoad();
+      return event;
+    }
+    console.log(checkStatResult.error.code);
+    if(checkStatResult.error.code=='checkStat_returnComp' && !event.record.warrantyStatus.value){
+      event.error='故障品状態が空欄です'
+      event.record.warrantyStatus.error = '空欄です';
+      endLoad();
+      return event;
+    }
+
+    // シリアル番号の品質区分を入れる
+    let newDeviceList = await updateQuality(event.record.deviceList.value)
+    if(!newDeviceList.result){
+      event.error = 'シリアル番号が入力されていません';
+      console.log(newDeviceList);
+      endLoad();
+      return event;
+    }
+    event.record.deviceList.value = newDeviceList.resp;
+    endLoad();
+    return event;
+  });
+
+  kintone.events.on('app.record.create.submit.success',async function(event) {
+    startLoad();
+    // 状態確認
+    let checkStatResult = checkStat(
+      event.record.working_status.value,
+      event.record.syncStatus_batch.value,
+      event.record.application_type.value
+    );
+    // 状態が例外だった場合処理を中止
+    if(!checkStatResult.result){
+      endLoad();
+      return event;
+    }
+
+    // ステータス更新内容
+    let putBody_workStat = {
+      'app': kintone.app.getId(),
+      'id': event.record.$id.value,
+      'record': {}
+    };
+
+    // シリアル連携
+    let sNumLinkResult
+    try{
+      sNumLinkResult = await sNumLink(event)
+      if(!sNumLinkResult.result){
+        console.log(sNumLinkResult);
+        await returnWorkStat(event);
+        putBody_workStat.record.syncStatus_serial={
+          value:'error'
+        }
+        await changeStatus(putBody_workStat)
+        endLoad();
+        return event;
+      } else {
+        putBody_workStat.record.syncStatus_serial={
+          value:'success'
+        }
+      }
+      console.log('シリアル連携に成功しました');
+    } catch(e){
+      alert('シリアル連携で不明なエラーが発生しました');
+      console.log(e);
+      endLoad();
+      return event;
+    }
+
+    // 在庫連携
+    if(event.record.syncStatus_stock.value!='success' && sNumLinkResult.resp){
+      let result_stockCTL
+      if(checkStatResult.error.code=='checkStat_returnComp'){
+        result_stockCTL = await ctl_stock_v2(event.record, sNumLinkResult.resp.shipData, null, 31);
+      } else if(checkStatResult.error.code=='checkStat_shippingComp') {
+        result_stockCTL = await ctl_stock_v2(event.record, sNumLinkResult.resp.shipData, 25, 31);
+      }
+      if(!result_stockCTL.result){
+        console.log(result_stockCTL.error);
+        await returnWorkStat(event);
+        putBody_workStat.record.syncStatus_stock={
+          value:'error'
+        }
+        await changeStatus(putBody_workStat)
+        endLoad();
+        return event;
+      } else {
+        putBody_workStat.record.syncStatus_stock={
+          value:'success'
+        }
+      }
+      console.log('在庫連携に成功しました');
+    }
+
+    // ステータス更新
+    let updateStatus = await changeStatus(putBody_workStat)
+    if(!updateStatus.result){
+      console.log(updateStatus);
+      endLoad();
+      return event;
+    }
+    alert('登録成功しました。\n次の作業に進めてください')
+    endLoad();
+    return event;
+  });
+
+  kintone.events.on('app.record.edit.submit.success',async function(event) {
+    startLoad();
+    // 状態確認
+    let checkStatResult = checkStat(
+      event.record.working_status.value,
+      event.record.syncStatus_batch.value,
+      event.record.application_type.value
+    );
+    // 状態が例外だった場合処理を中止
+    if(!checkStatResult.result){
+      endLoad();
+      return event;
+    }
+
+    // ステータス更新内容
+    let putBody_workStat = {
+      'app': kintone.app.getId(),
+      'id': event.record.$id.value,
+      'record': {}
+    };
+
+    // シリアル連携
+    let sNumLinkResult
+    try{
+      sNumLinkResult = await sNumLink(event)
+      if(!sNumLinkResult.result){
+        console.log(sNumLinkResult);
+        await returnWorkStat(event);
+        putBody_workStat.record.syncStatus_serial={
+          value:'error'
+        }
+        await changeStatus(putBody_workStat)
+        endLoad();
+        return event;
+      } else {
+        putBody_workStat.record.syncStatus_serial={
+          value:'success'
+        }
+        console.log('シリアル連携に成功しました');
+      }
+    } catch(e){
+      alert('シリアル連携で不明なエラーが発生しました');
+      console.log(e);
+      endLoad();
+      return event;
+    }
+    console.log(sNumLinkResult);
+
+    // 在庫連携
+    if(event.record.syncStatus_stock.value!='success' && sNumLinkResult.resp){
+      let result_stockCTL
+      if(checkStatResult.error.code=='checkStat_returnComp'){
+        result_stockCTL = await ctl_stock_v2(event.record, sNumLinkResult.resp.shipData, null, 31);
+      } else if(checkStatResult.error.code=='checkStat_shippingComp') {
+        result_stockCTL = await ctl_stock_v2(event.record, sNumLinkResult.resp.shipData, 25, 31);
+      }
+      if(!result_stockCTL.result){
+        console.log(result_stockCTL.error);
+        await returnWorkStat(event);
+        putBody_workStat.record.syncStatus_stock={
+          value:'error'
+        }
+        await changeStatus(putBody_workStat)
+        endLoad();
+        return event;
+      } else {
+        putBody_workStat.record.syncStatus_stock={
+          value:'success'
+        }
+      }
+      console.log('在庫連携に成功しました');
+    }
+
+    // ステータス更新
+    let updateStatus = await changeStatus(putBody_workStat)
+    if(!updateStatus.result){
+      console.log(updateStatus);
+      endLoad();
+      return event;
+    }
+    alert('登録成功しました。\n次の作業に進めてください')
+    endLoad();
+    return event;
+  });
+})();
+
+// シリアル番号の情報を取得し、品質区分をreturn
+async function updateQuality(deviceList){
+  try{
+    for(const list of deviceList){
+      let snumRecord = (await getRecords({app: sysid.DEV.app_id.sNum,filterCond: 'sNum like "' + list.value.sNum.value + '"'})).records;
+      if(snumRecord.length==0){
+        // return {result: false, error: {target: 'updateQuality', code: 'updateQuality_notSnum'}};
+        list.value.qualityClass.value = '新品'
+      } else {
+        list.value.qualityClass.value = snumRecord[0].sState.value
+      }
+    }
+  } catch(e){
+    console.log(e);
+    alert('シリアル番号の品質取得の際にエラーが発生しました。');
+    return {result: false, error: {target: 'updateQuality', code: 'updateQuality_error'}};
+  }
+  return {result: true, resp: deviceList, error: {target: 'updateQuality', code: 'updateQuality_success'}};
+}
+
+function checkStat(status, batch, applicationType){
+  // エラー処理
+  if(batch=='error'||batch==''){
+    alert('デバイス登録確認がエラーか空欄です。');
+    return {result: false, error: {target: 'checkStat', code: 'checkStat_error-syncStatus_batch'}};
+  }
+  // if(applicationType.match(/故障交換/) && status.match(/出荷完了|着荷完了/)){
+  //   return {result: false, error: {target: 'checkStat', code: 'checkStat_error-brokenExchange-badStatus'}};
+  // }
+  // ステータス確認
+  if(status=='故障品返却完了' && applicationType.match(/故障交換/)){
+    return {result: true, error: {target: 'checkStat', code: 'checkStat_returnComp'}};
+  }else if(status=='出荷完了'){
+    return {result: true, error: {target: 'checkStat', code: 'checkStat_shippingComp'}};
+  }
+  return {result: false, error: {target: 'checkStat', code: 'checkStat_unknown'}};
+}
+
+async function sNumLink(event){
+  try{
+    let result_snCTL
+    if(event.record.syncStatus_serial.value=='success'){
+      alert('シリアル連携は完了済みです');
+    } else{
+      if(event.record.ship_number.value=='') {
+        alert('伝票番号が記入されていません。');
+        return {result: false, error:  {target: '伝票番号', code: 'notRequireData'}};
+      }
+      if(event.record.shipping_datetime.value==''){
+        alert('出荷日時が記入されていません。');
+        return {result: false, error:  {target: '出荷日時', code: 'notRequireData'}};
+      }
+      let sninfo = renew_sNumsInfo_alship_forDelivery(event.record, 'deviceList');
+      if(sninfo.shipInfo.deviceInfo.length > 0){
+        result_snCTL = await ctl_sNumv2('internal', sninfo);
+        console.log(result_snCTL);
+        if(!result_snCTL.result){
+          alert(event.error = result_snCTL.error.target + ': ' + errorCode[result_snCTL.error.code]);
+          return result_snCTL;
+        }
+      } else {
+        alert('連携するシリアル番号がありません');
+        return {result: false, error: {target: 'sNumLink', code: 'sn_nosnum'}};
+      }
+    }
+    return {result: true, resp: result_snCTL, error: {target: 'sNumLink', code: 'sNumLink_success'}};
+  } catch(e){
+    alert('シリアル連携で不明なエラーが発生しました');
+    return {result: false, message: e, error: {target: 'シリアル連携', code: 'unknownError'}};
+  }
+}
+
+// 処理が中止した場合作業ステータスを集荷待ちに変更
+async function returnWorkStat(event){
+  let updateJson = {
+    app: kintone.app.getId(),
+    id: event.record.$id.value,
+    record:{
+      working_status:{
+        value: '集荷待ち'
+      }
+    }
+  }
+  let putResult = await kintone.api(kintone.api.url('/k/v1/record.json', true), 'PUT', updateJson)
+    .then(function (resp) {
+      return {
+        result: true,
+        message: resp
+      };
+    }).catch(function (error) {
+      return {
+        result: false,
+        message: error
+      };
+    });
+  if(!putResult.result){
+    alert('作業ステータスの差し戻しに失敗しました');
+    return {result: false, error: {target: 'returnWorkStat', code: 'returnWorkStat_updateError'}};
+  }
+
+  return {result: true, error: {target: 'returnWorkStat', code: 'returnWorkStat_success'}};
+}
+
+// 処理が終了した場合各種ステータスを変更
+async function changeStatus(updateBody){
+  let updateStatus = await kintone.api(kintone.api.url('/k/v1/record.json', true), "PUT", updateBody)
+  .then(function (resp) {
+    return {result: true, resp:resp, error: {target: 'changeStatus', code: 'changeStatus_errorUpdateStatus'}};
+  }).catch(function (error) {
+    console.log(error);
+    alert('ステータス更新に失敗しました');
+    return {result: false, error: {target: 'changeStatus', code: 'changeStatus_errorUpdateStatus'}};
+  });
+  return updateStatus
+}
