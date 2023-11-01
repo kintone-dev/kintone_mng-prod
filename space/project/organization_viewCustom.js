26df100162ba84590e3966347be2c4ac85cdc361
kintone-mng <89760706+kintone-mng@users.noreply.github.com>
2023-11-02 01:50:05 +0900

    Update organization_viewCustom.js

--- space/project/organization_viewCustom.js
+++ space/project/organization_viewCustom.js
@@ -1,25 +1,6 @@
 (function () {
   'use strict';
 
-  /*
-  kintone.events.on('app.record.detail.show', function (event) {
-    //クリップボードに請求先をコピーする
-    var copy_cName = document.createElement('button');
-    copy_cName.id = 'btn_copy_cName';
-    copy_cName.innerText = '請求先コピー';
-    copy_cName.onclick = function () {
-      var clientName = (event.record.cName.value);
-      if (execCopy(clientName)) {
-        console.log('顧客名をコピーしました。\n案件管理の「請求先検索」に貼り付けてください。');
-      } else {
-        console.log('このブラウザでは対応していません。\n手動で顧客名をコピーするか、案件管理で請求先をもう一度検索してください。');
-      }
-    };
-    kintone.app.record.getSpaceElement('btn_copy_cName').appendChild(copy_cName);
-    return event;
-  });
-  */
-
   kintone.events.on(['app.record.create.change.cName', 'app.record.edit.change.cName'], function (event) {
     event.record.BMC.value = event.record.cName.value;
     event.record.RRMC.value = event.record.cName.value;
@@ -29,20 +10,26 @@
    * Adminパスワード自動生成機能
    */
   kintone.events.on(['app.record.create.show', 'app.record.edit.show'], function (event){
-    // パスワード自動生成ボタン
-    let createAdminPassword = setBtn('btn_createAdminPassword', 'パスワード生成');
-    $('#' + createAdminPassword.id).on('click', function () {
-      let eRecord = kintone.app.record.get();
-      eRecord.record.adminPassword.value = pw_generator(12);
-      kintone.app.record.set(eRecord);
-    });
-    // 組織コード自動生成ボタン
-    let createOrgCode = setBtn('btn_createOrgCode', '組織コード生成');
+    // 組織コード自動生成
+    // event.record.orgCode.value = string_generator(8);
+    // 組織コード生成ボタン
+    let createOrgCode = setBtn('btn_createOrgCode', '組織コード生成/更新');
     $('#' + createOrgCode.id).on('click', function () {
       let eRecord = kintone.app.record.get();
       eRecord.record.orgCode.value = string_generator(8);
       kintone.app.record.set(eRecord);
     });
+
+    // パスワード自動生成
+    event.record.adminPassword.value = pw_generator(12);
+    // パスワード生成ボタン
+    let createAdminPassword = setBtn('btn_createAdminPassword', 'パスワード更新');
+    $('#' + createAdminPassword.id).on('click', function () {
+      let eRecord = kintone.app.record.get();
+      eRecord.record.adminPassword.value = pw_generator(12);
+      kintone.app.record.set(eRecord);
+    });
+
     // パスワード生成ツール(Adminパスワード横)
     let createPW_header = setBtn('btn_createAdminPassword', 'パスワード生成ツール');
     $('#' + createPW_header.id).on('click', function () {
@@ -53,6 +40,7 @@
       `<p><input type="text" id="generatedPassword" value="${pw_generator(12)}" /></p>`;
       $('#mwFrame').fadeIn();
     });
+    return event;
   });
   // パスワード生成ツール(header)
   // kintone.events.on('app.record.detail.show', function (event){
